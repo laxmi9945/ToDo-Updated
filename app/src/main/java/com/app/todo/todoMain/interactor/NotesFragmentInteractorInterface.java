5066c246c08d540e51465cfc207222feeec65448
Laxmi Mishra <laxmi9945@gmail.com>
2017-05-15 19:00:51 +0530

    15/05/2017 todo app

---
+++ app/src/main/java/com/app/todo/todoMain/interactor/NotesFragmentInteractorInterface.java
@@ -0,0 +1,8 @@
+package com.app.todo.todoMain.interactor;
+
+/**
+ * Created by bridgeit on 13/5/17.
+ */
+
+public interface NotesFragmentInteractorInterface {
+}
---
+++ app/src/main/java/com/app/todo/todoMain/ui/fragment/NotesFragmentInterface.java
@@ -0,0 +1,8 @@
+package com.app.todo.todoMain.ui.fragment;
+
+/**
+ * Created by bridgeit on 13/5/17.
+ */
+
+public interface NotesFragmentInterface {
+}
